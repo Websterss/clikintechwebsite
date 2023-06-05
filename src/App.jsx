16e54e68e31fb79e43cb9b0385fb12be1f335c69
RinzlerN26 <Email>
2023-06-05 20:23:33 +0530

    g

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './components/Welcome'
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+    <Welcome/>
+    </div>
+  );
+}
+export default App;
