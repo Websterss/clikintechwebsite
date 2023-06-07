06e9441b908b9cb3a529d6c2aede48c2f57b3125
ankita <ankitarajpoot@gmail.com>
2023-06-07 19:49:19 +0530

    my-coding

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import './components/Welcome'
 import Welcome from './components/Welcome';
 import Welcome2 from './components/Welcome2';
 import Footer from './components/Footer';
+import Contact_us from './component2/Contact_us';
 
 function App() {
   return (
     <div className="App">
-    <Welcome/>
-    <Welcome2/>
-    <Footer/>
+      <Welcome />
+      <Welcome2 />
+      <Contact_us />
+
+      <Footer />
     </div>
   );
 }
