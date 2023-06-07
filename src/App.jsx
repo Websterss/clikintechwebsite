f630d7bce71e3990a5d2cb01096367f0e842d1ab
Websterss <clikintechweb@gmail.com>
2023-06-07 15:23:51 +0530

    hero and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import './components/Welcome'
 import Welcome from './components/Welcome';
+import Welcome2 from './components/Welcome2';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
     <Welcome/>
+    <Welcome2/>
+    <Footer/>
     </div>
   );
 }
