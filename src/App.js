24c6e09cf830dbb1f2d0f5c8a34bbff76dc04013
Websterss <clikintechweb@gmail.com>
2023-06-08 11:18:25 +0530

    add topbar and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom/cjs/react-router-dom'; 
+import TopBar from './Components/TopBar/TopBar';
+
 
 function App() {
   return (
     <div className="App">
-    Hello World
+      <Router>
+      <TopBar />
+      <Navbar />
+      <Switch>
+
+        </Switch>
+      </Router>
+ 
     </div>
   );
 }
