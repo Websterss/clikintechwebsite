58fbbecc15132a1ac53d38c6f7b1ffa4c5afe21e
Websterss <clikintechweb@gmail.com>
2023-06-08 11:56:24 +0530

    all updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Welcome from '../Welcome'
 import Welcome2 from '../Welcome2'
 import Footer from '../Footer'
+import Contact_us from '../../component2/Contact_us'
 
 function Home() {
   return (
     <div>
         <Welcome />
         <Welcome2 />
+        <Contact_us />
         <Footer />
+       
     </div>
   )
 }
