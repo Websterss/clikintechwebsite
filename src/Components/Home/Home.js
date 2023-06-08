d715db3a6929248c0050e0df2ba96ebc25c52867
Websterss <clikintechweb@gmail.com>
2023-06-08 11:30:38 +0530

    add home page

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Welcome from '../Welcome'
+import Welcome2 from '../Welcome2'
+import Footer from '../Footer'
+
+function Home() {
+  return (
+    <div>
+        <Welcome />
+        <Welcome2 />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
