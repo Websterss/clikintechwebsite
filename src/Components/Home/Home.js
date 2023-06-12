2cc46dcdf58b8e75bc919c7efe5b84217d4bbed0
Websterss <clikintechweb@gmail.com>
2023-06-12 09:32:57 +0530

    update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,15 +3,16 @@ import Welcome from '../Welcome'
 import Welcome2 from '../Welcome2'
 import Footer from '../Footer'
 import Contact_us from '../../component2/Contact_us'
+import Our_Services from '../Our_Services/Our_Services'
 
 function Home() {
   return (
     <div>
-        <Welcome />
-        <Welcome2 />
+         <Welcome />
+        <Welcome2 /> 
+        <Our_Services />
         <Contact_us />
-        <Footer />
-       
+     <Footer />
     </div>
   )
 }
