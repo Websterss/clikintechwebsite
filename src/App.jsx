bb5b9d2990fb79f65f721132b3759f54c80a4017
Websterss <clikintechweb@gmail.com>
2023-06-13 13:37:20 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import './App.css';
 import { BrowserRouter as Router , Switch, Route } from 'react-router-dom/cjs/react-router-dom'; 
 import TopBar from './component/TopBar.js/TopBar';
 import Home from './component/Home/Home';
-import Services from './component/Services/Services';
-import Career from './component/Career/Career';
-import Blog from './component/Blog/Blog';
-import AboutUs from './component/About Us/AboutUs';
-import ContactUs from './component/Contact Us/ContactUs';
-import Footer from './component/Footer/Footer';
 import Navbar from './component/Navbar/Navbar';
 
 
@@ -20,13 +14,8 @@ function App() {
         <Navbar />
       <Switch>
          <Route exact path="/" component={Home} />
-         <Route exact path="/Service" component={Services} />
-         <Route exact path="/Career" component={Career} />
-         <Route exact path="/Blog" component={Blog} />
-         <Route exact path="/About Us" component={AboutUs} />
-         <Route exact path="/Contact Us" component={ContactUs} />
       </Switch>
-      <Footer />
+
       </Router>
  
     </div>
--- src/component/Career/Career.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Career() {
-  return (
-    <div>Career</div>
-  )
-}
-
-export default Career
