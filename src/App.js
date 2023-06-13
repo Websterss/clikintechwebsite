532c8abdd3fd7b07bef6331f761253b9760ad675
Websterss <134377809+Websterss@users.noreply.github.com>
2023-06-13 11:40:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-import Navbar from './components/Home/Navbar/Navbar';
+
 import { BrowserRouter as Router , Switch, Route } from 'react-router-dom/cjs/react-router-dom'; 
-import TopBar from './components/Home/TopBar/TopBar';
-import Home from './components/Home/Home';
+
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <TopBar />
-      <Navbar />
       <Switch>
-     <Route exact path="/"  component={Home}/>
+    
         </Switch>
       </Router>
  
