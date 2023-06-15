d5524d9444020cb4eb369393a7a7453026ecb7b0
Websterss <134377809+Websterss@users.noreply.github.com>
2023-06-15 15:03:01 +0530

    Update Footer.jsx

--- src/component/Home/Footer.jsx
+++ src/component/Home/Footer.jsx
@@ -19,74 +19,61 @@ function Footer(){
   };
     return(
         <div>
-        <footer className="footer">
-        <img src={logo} alt="Logo" className="footer-logo" />
-        <div className="footer-menu">
-          <ul className="footer-menu-column">
-            <li>
-                <li class="footer-title">We are a digital<br></br>marketing agency</li>
-                <IconContext.Provider value={{ className: "top-react-icons" }}> 
-                <FaInstagram onClick={openInstagram}/>
-                <FaFacebook onClick={openFacebook}/>
-                <FaTwitter onClick={opentwitter}/>
-                <FaGithub onClick={opengithub}/>
-                </IconContext.Provider>  
-            </li>
-          </ul>
-          <ul className="footer-menu-column">
-            <li>
-              <a href=""><strong>Services</strong></a>
-            </li>
-            <li>
-              <a href="">Digital marketing</a>
-            </li>
-            <li>
-              <a href="">Web development</a>
-            </li>
-            <li>
-              <a href="">Mobile App Development</a>
-            </li>
-          </ul>
-          <ul className="footer-menu-column">
-            <li>
-              <a href=""><strong>Home</strong></a>
-            </li>
-            <li>
-              <a href="">Career</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Services</a>
-            </li>
-            <li>
-              <a href="">Contact Us</a>
-            </li>
-          </ul>
-          <ul className="footer-menu-column">
-            <li><strong>Resources</strong></li>
-            <li>
-              <a href="">Support</a>
-            </li>
-            <li>
-              <a href="">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="">Terms & Conditions</a>
-            </li>
-          </ul>
-          <ul className="footer-menu-column">
-            <li><strong>Contact</strong></li>
-            <li>Barrackpore</li>
-            <li>Kolkata- 122</li> 
-            <li>West Bengal</li>
-          </ul>
-          
+      <footer className="footer">
+        <div className="container">
+          <div className="row ">
+            <div className="col-md-2">
+              <img src={logo} alt="Logo" className="footer-logo mb-4" />
+              <h5 className="footer-title">We are a digital<br></br> marketing agency</h5>
+              <IconContext.Provider value={{ className: 'top-react-icons' }}>
+                <FaInstagram onClick={openInstagram} />
+                <FaFacebook onClick={openFacebook} />
+                <FaTwitter onClick={opentwitter} />
+                <FaGithub onClick={opengithub} />
+              </IconContext.Provider>
+            </div>
+            <div className="col-md-3">
+              
+              <ul className="list-unstyled">
+                <li><h5><strong>Services</strong></h5></li>
+                <li><a href="/">Digital marketing</a></li>
+                <li><a href="/">Web development</a></li>
+                <li><a href="/">Mobile App Development</a></li>
+              </ul>
+            </div>
+            <div className="col-md-2">
+              
+              <ul className="list-unstyled">
+                <li><h5><strong>Home</strong></h5></li>
+                <li><a href="/">Career</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Contact Us</a></li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              
+              <ul className="list-unstyled">
+                <li><h5><strong>Resources</strong></h5></li>
+                <li><a href="/">Support</a></li>
+                <li><a href="/">Privacy Policy</a></li>
+                <li><a href="/">Terms & Conditions</a></li>
+              </ul>
+            </div>
+            <div className="col-md-2">
+              
+              <ul className="list-unstyled">
+                <li><h5><strong>Contact</strong></h5></li>
+                <li>Barrackpore</li>
+                <li>Kolkata- 122</li>
+                <li>West Bengal</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </footer>
-      <p class="copyright">Copyright 2023, All Rights Reserved by Clikin Tech</p>
-      </div>
+      <p className="text-center text-dark bg-light py-4 mb-0">Copyright 2023, All Rights Reserved by Clikin Tech</p>
+    </div>
     
     
     );
