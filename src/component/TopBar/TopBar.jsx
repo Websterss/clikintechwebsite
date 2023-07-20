ebcbac533900a611ec3c120e21ab38580d3e0401
Websterss <clikintechweb@gmail.com>
2023-07-20 13:04:01 +0530

    updated

--- src/component/TopBar/TopBar.jsx
+++ src/component/TopBar/TopBar.jsx
@@ -5,7 +5,7 @@ function TopBar() {
   return (
     <div className='topbar'>
       <p className='topbar_p'>It’s not just a work, it’s an Experience</p>
-      <button className='topbar-button'>Portfolio</button>
+      <a href="https://www.behance.net/clikin-tech" target='blank' className='topbar-link'>Portfolio</a>
     </div>
   )
 }
