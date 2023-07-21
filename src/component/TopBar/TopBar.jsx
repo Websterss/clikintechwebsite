28e32d25b8c8477bb04363a9cbf7fd427cdb5dbc
MANYAJAIN195 <manyajain195@gmail.com>
2023-07-21 18:04:41 +0530

    responsive contact us

--- src/component/TopBar/TopBar.jsx
+++ src/component/TopBar/TopBar.jsx
@@ -5,7 +5,7 @@ function TopBar() {
   return (
     <div className='topbar'>
       <p className='topbar_p'>It’s not just a work, it’s an Experience</p>
-      <a href="https://www.behance.net/clikin-tech" target='blank' className='topbar-link'>Portfolio</a>
+      <a href="https://www.behance.net/clikin-tech" target='blank' className='topbar-button'>Portfolio</a>
     </div>
   )
 }
