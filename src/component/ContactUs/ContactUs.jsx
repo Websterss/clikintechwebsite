bbc922a64768aab8556f463cb133183726c3314b
Websterss <clikintechweb@gmail.com>
2023-07-22 12:30:32 +0530

    all update

--- src/component/ContactUs/ContactUs.jsx
+++ src/component/ContactUs/ContactUs.jsx
@@ -1,15 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useState,useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
+import "./ContactUs.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 import image from '../../res/HomeImages/contact.png';
 import { IoMdMail } from 'react-icons/io';
 import { TiLocation } from "react-icons/ti";
 import { ImPhone } from "react-icons/im";
 
 
-const Contact_us = () => {
+const ContactUs = () => {
+
+  const [formObject, setFormObject] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+  const validation = yup.object().shape({
+    name: yup.string().min(2).max(15).required("Please enter your name"),
+    email: yup
+      .string()
+      .required("Please enter your email")
+      .email("Please enter a valid email"),
+    phone: yup.string().matches(phoneRegExp, "Phone number is not valid"),
+    message: yup
+      .string()
+      .min(10)
+      .max(200),
+  });
 
   const form = useRef();
 
@@ -26,51 +49,84 @@ const Contact_us = () => {
 
   return (
     <div>
-
-       
+      <Formik
+        initialValues={formObject}
+        validationSchema={validation}
+        validateOnBlur={false}
+        validateOnChange={false}
+        onSubmit={(values, actions) => {
+          // Handle form submission
+        }}
+      >
+        <Form ref={form} onSubmit={sendEmail}>
           <div className="container">
             <article> <h1>Get in touch</h1>
             <hr className='line'></hr>
             <p className="para">We are here for you! How can we help?</p></article>
-           
             <div className="row">
               <div className="col-md-6">
-              <form ref={form} onSubmit={sendEmail}>
-              <input
-                   type="text"
+                <div className="form-group">
+                  <Field
+                    type="text"
                     name="user_name"
-                     placeholder="Enter your name"
-                     className="form-control"                 
-                   required/>
-    <input
-                    type="email"
-                   name="user_email"
-                     placeholder="Enter your email"
-                     className="form-control"                 
-                required   />
-
-<input
-                    type="number"
+                    placeholder="Enter your name"
+                    className="form-control"                 
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+                <div className="form-group">
+                  <Field
+                    type="text"
+                    name="user_email"
+                    placeholder="Enter your email address"
+                    className="form-control"
+                  
+                  />
+                  <ErrorMessage
+                    name="user_email"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+                <div className="form-group">
+                  <Field
+                    type="text"
                     name="phone"
-                     placeholder="Enter your phone"
-                     className="form-control"                 
-                  required />
-
-                 <textarea
-                     type='text'
-                     name="message"
-                     placeholder="Enter your message"
-                                          className="form-control"
-                 style={{
+                    placeholder="Enter your phone number"
+                    className="form-control"
+      
+                  />
+                  <ErrorMessage
+                    name="phone"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+                <div className="form-group">
+                  <Field
+                    as="textarea"
+                    name="message"
+                    placeholder="Enter your message"
+                    className="form-control"
+                    style={{
                       height: "100px", 
-                       border: "none", 
-                       backgroundColor: "rgb(241, 237, 237)", 
-                     }} required/>
+                      border: "none", 
+                      backgroundColor: "rgb(241, 237, 237)", 
+                    }}
+                  />
+                  <ErrorMessage
+                    name="message"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
                 <button type="submit" className="btn btn-primary">
                   Submit
                 </button>
-                </form>
-                
               </div>
               <div className="col-md-6 img-container">
                 <img src={image} alt="Contact" className="img-fluid" />
@@ -83,10 +139,10 @@ const Contact_us = () => {
               </div>
             </div>
           </div>
-      
-      {/* </Formik> */}
+        </Form>
+      </Formik>
     </div>
   );
 };
 
-export default Contact_us;
+export default ContactUs;
